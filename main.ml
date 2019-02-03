ba4d249edda35787754f189e0a0d53b503dcadc9
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-02-03 23:09:33 +0100

    Cleanup

--- main.ml
+++ main.ml
@@ -114,21 +114,8 @@ let db =
     dbuser = Some "root";
     dbsocket = None }
 
-let connect () =
-  try
-    Mysql.connect db
-  with Mysql.Error s ->
-    prerr_endline s;
-    exit 2
-
 let exec dbd ~f query =
-  match Mysql.exec dbd query with
-  | exception Mysql.Error s ->
-      prerr_endline s;
-      Mysql.disconnect dbd;
-      exit 2
-  | result ->
-      Mysql.map result ~f
+  Mysql.map (Mysql.exec dbd query) ~f
 
 let timestamp s =
   let {Unix.tm_year; tm_mon; tm_mday; tm_hour; tm_min; tm_sec; _} =
@@ -137,14 +124,13 @@ let timestamp s =
   Printf.sprintf "%04d-%02d-%02dT%02d:%02d:%02dZ"
     (tm_year + 1900) (tm_mon + 1) tm_mday tm_hour tm_min tm_sec
 
-let () =
-  let dbd = connect () in
+let main dbd =
   let categories =
     let f = function
       | [|Some id; Some name|] ->
-          (int_of_string id, name)
+          int_of_string id, name
       | _ ->
-          failwith ""
+          failwith "Unexpected response when querying categories"
     in
     let r = exec dbd ~f "SELECT id, name FROM mantis_category_table;" in
     Hashtbl.of_seq (List.to_seq r)
@@ -156,13 +142,13 @@ let () =
             match Hashtbl.find_opt user_map user_name with
             | Some s -> s
             | None ->
-                Printf.eprintf "WARNING: user %S does not have a GH user associated to it\n%!"
+                Printf.eprintf "WARNING: user_map: missing user: %S\n%!"
                   user_name;
                 user_name ^ "@Mantis"
           in
           int_of_string id, gh_user
       | _ ->
-          failwith ""
+          failwith "Unexpected response when querying users"
     in
     let r =
       exec dbd ~f "SELECT id, username FROM mantis_user_table;"
@@ -172,9 +158,9 @@ let () =
   let texts =
     let f = function
       | [|Some id; Some description; Some steps_to_reproduce; Some additional_information|] ->
-          (int_of_string id, (description, steps_to_reproduce, additional_information))
+          int_of_string id, (description, steps_to_reproduce, additional_information)
       | _ ->
-          failwith ""
+          failwith "Unexpected response when querying bug texts"
     in
     let r =
       exec dbd ~f "SELECT id, description, steps_to_reproduce, additional_information \
@@ -186,9 +172,9 @@ let () =
     let texts =
       let f = function
         | [|Some id; Some note|] ->
-            (int_of_string id, note)
+            int_of_string id, note
         | _ ->
-            failwith ""
+            failwith "Unexpected response when querying notes texts"
       in
       let r = exec dbd ~f "SELECT id, note FROM mantis_bugnote_text_table;" in
       hashtbl_of_list r
@@ -202,7 +188,7 @@ let () =
           let date_submitted = timestamp date_submitted in
           int_of_string bug_id, {Note.reporter; text; last_modified; date_submitted}
       | _ ->
-          failwith ""
+          failwith "Unexpected response when querying notes"
     in
     let r =
       exec dbd ~f "SELECT bug_id, reporter_id, bugnote_text_id, \
@@ -234,11 +220,28 @@ let () =
           description; steps_to_reproduce; additional_information; target_version;
           notes }
     | _ ->
-        failwith "unxpected"
+        failwith "Unexpected response when querying bugs"
   in
-  let issues = exec dbd ~f query in
-  let f issue =
-    let json = Issue.to_json issue in
-    Printf.printf "%a\n" (Yojson.pretty_to_channel ~std:true) json
-  in
-  List.iter ~f issues
+  exec dbd ~f query
+
+let connect () =
+  try
+    Mysql.connect db
+  with Mysql.Error s ->
+    prerr_endline s;
+    exit 2
+
+let () =
+  let dbd = connect () in
+  match main dbd with
+  | issues ->
+      Mysql.disconnect dbd;
+      let f issue =
+        let json = Issue.to_json issue in
+        Printf.printf "%a\n" (Yojson.pretty_to_channel ~std:true) json
+      in
+      List.iter ~f issues
+  | exception e ->
+      Printf.eprintf "ERROR: %s\n%!" (Printexc.to_string e);
+      Mysql.disconnect dbd;
+      exit 2
