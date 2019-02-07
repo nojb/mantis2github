3fba919d3bc5e14bc330c6fd1161c5898850284f
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-02-07 22:47:26 +0100

    Begin gist support

--- github.ml
+++ github.ml
@@ -32,61 +32,63 @@ type t =
     token: string option;
   }
 
-let root = "https://api.github.com"
+module Api = struct
+  let root = "https://api.github.com"
 
-let curl ?(verbose = false) meth ?(headers = []) ?data ?(params = []) {owner; repo; token} route =
-  let headers =
-    match token with
-    | Some token -> ("Authorization", "token " ^ token) :: headers
-    | None -> headers
-  in
-  let headers = ("Accept", "application/vnd.github.golden-comet-preview+json") :: headers in
-  let cmd =
+  let curl ?(verbose = false) meth ?(headers = []) ?data ?(params = []) {owner; repo; token} route =
     let headers =
-      match headers with
-      | [] -> ""
-      | l ->
-          String.concat " "
-            (List.map (fun (k, v) -> "-H \"" ^ k ^ ": " ^ v ^ "\"") l) ^ " "
+      match token with
+      | Some token -> ("Authorization", "token " ^ token) :: headers
+      | None -> headers
     in
-    let data =
-      match data with
-      | None -> ""
-      | Some data ->
-          (* Printf.eprintf "%a\n%!" (Yojson.Basic.pretty_to_channel ~std:true) data; *)
-          let filename, oc = Filename.open_temp_file "curl" "data" in
-          set_binary_mode_out oc true;
-          Yojson.Basic.pretty_to_channel ~std:true oc data;
-          close_out oc;
-          "--data-binary @" ^ filename ^ " "
+    let headers = ("Accept", "application/vnd.github.golden-comet-preview+json") :: headers in
+    let cmd =
+      let headers =
+        match headers with
+        | [] -> ""
+        | l ->
+            String.concat " "
+              (List.map (fun (k, v) -> "-H \"" ^ k ^ ": " ^ v ^ "\"") l) ^ " "
+      in
+      let data =
+        match data with
+        | None -> ""
+        | Some data ->
+            (* Printf.eprintf "%a\n%!" (Yojson.Basic.pretty_to_channel ~std:true) data; *)
+            let filename, oc = Filename.open_temp_file "curl" "data" in
+            set_binary_mode_out oc true;
+            Yojson.Basic.pretty_to_channel ~std:true oc data;
+            close_out oc;
+            "--data-binary @" ^ filename ^ " "
+      in
+      let params =
+        match params with
+        | [] -> ""
+        | l -> "?" ^ String.concat "&" (List.map (fun (k, v) -> String.concat "=" [k; v]) l)
+      in
+      let verbose = if verbose then "--verbose " else "" in
+      Printf.sprintf "curl -Ss %s%s%s-X %s %s/repos/%s/%s/%s%s"
+        verbose data headers meth root owner repo route params
     in
-    let params =
-      match params with
-      | [] -> ""
-      | l -> "?" ^ String.concat "&" (List.map (fun (k, v) -> String.concat "=" [k; v]) l)
-    in
-    let verbose = if verbose then "--verbose " else "" in
-    Printf.sprintf "curl -Ss %s%s%s-X %s %s/repos/%s/%s/%s%s"
-      verbose data headers meth root owner repo route params
-  in
-  if verbose then Printf.eprintf "+ %s\n%!" cmd;
-  let tmp = Filename.temp_file "curl" "out" in
-  assert (Sys.command (Printf.sprintf "%s > %s" cmd tmp) = 0);
-  let ic = open_in_bin tmp in
-  let s = really_input_string ic (in_channel_length ic) in
-  close_in ic;
-  let json = Yojson.Basic.from_string s in
-  (* Printf.eprintf "%a\n%!" (Yojson.Basic.pretty_to_channel ~std:true) json; *)
-  match J.member "message" json with
-  | `String _ ->
-      let json = ["cmd", `String cmd; "res", json] in
-      let json = match data with None -> json | Some data -> ("data", data) :: json in
-      Error (`Assoc json)
-  | _ ->
-      Ok json
+    if verbose then Printf.eprintf "+ %s\n%!" cmd;
+    let tmp = Filename.temp_file "curl" "out" in
+    assert (Sys.command (Printf.sprintf "%s > %s" cmd tmp) = 0);
+    let ic = open_in_bin tmp in
+    let s = really_input_string ic (in_channel_length ic) in
+    close_in ic;
+    let json = Yojson.Basic.from_string s in
+    (* Printf.eprintf "%a\n%!" (Yojson.Basic.pretty_to_channel ~std:true) json; *)
+    match J.member "message" json with
+    | `String _ ->
+        let json = ["cmd", `String cmd; "res", json] in
+        let json = match data with None -> json | Some data -> ("data", data) :: json in
+        Error (`Assoc json)
+    | _ ->
+        Ok json
 
-let get ?verbose ?headers ?params gh route = curl ?verbose "GET" ?headers ?params gh route
-let post ?verbose ?headers ?data gh route = curl ?verbose "POST" ?headers ?data gh route
+  let get ?verbose ?headers ?params gh route = curl ?verbose "GET" ?headers ?params gh route
+  let post ?verbose ?headers ?data gh route = curl ?verbose "POST" ?headers ?data gh route
+end
 
 module Milestone = struct
   let list ?verbose gh =
@@ -95,7 +97,7 @@ module Milestone = struct
       let number = json |> J.member "number" |> J.to_int in
       Some (title, number)
     in
-    match get ?verbose ~params:["state", "all"] gh "milestones" with
+    match Api.get ?verbose ~params:["state", "all"] gh "milestones" with
     | Error json ->
         Printf.eprintf "%a\n%!" (Yojson.Basic.pretty_to_channel ~std:true) json;
         None
@@ -105,7 +107,7 @@ end
 
 module Issue = struct
   let is_imported ?verbose gh id =
-    match get ?verbose gh (Printf.sprintf "import/issues/%d" id) with
+    match Api.get ?verbose gh (Printf.sprintf "import/issues/%d" id) with
     | Error _ as x -> Some x
     | Ok json ->
         begin match json |> J.member "status" |> J.to_string with
@@ -127,7 +129,7 @@ module Issue = struct
         end
 
   let start_import ?verbose gh json =
-    match post ?verbose ~data:json gh "import/issues" with
+    match Api.post ?verbose ~data:json gh "import/issues" with
     | Ok json -> Ok (json |> J.member "id" |> J.to_int)
     | Error _ as x -> x
 
@@ -149,4 +151,31 @@ module Issue = struct
               loop (2 * n) (succ retries)
         in
         loop 1 0
+end
+
+module Gist = struct
+  type t =
+    {
+      files: (string * string) list;
+      description: string;
+      public: bool;
+    }
+
+  let to_json {files; description; public} =
+    let files =
+      List.map (fun (name, contents) ->
+          `Assoc [name, `Assoc ["contents", `String contents]]
+        ) files
+    in
+    `Assoc [ "files", `List files;
+             "description", `String description;
+             "public", `Bool public ]
+
+  let create ?verbose gh gist =
+    let data = to_json gist in
+    match Api.post ?verbose ~data gh "/gists" with
+    | Ok json ->
+        Ok (J.member "html_url" json |> J.to_string)
+    | Error _ as x ->
+        x
 end
