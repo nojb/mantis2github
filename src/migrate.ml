d195923d1287da4cbbdcef3e6af6d14e2699c0c0
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2019-02-08 13:24:34 +0000

    Fix rendering

--- src/migrate.ml
+++ src/migrate.ml
@@ -79,11 +79,6 @@ module Label = struct
   end
 end
 
-let badd buf title s =
-  let fence = String.make 6 '`' in
-  let s = String.trim s in
-  if s <> "" then Printf.bprintf buf "**%s**\n%s\n%s\n%s\n" title fence s fence
-
 let timestamp s =
   let {Unix.tm_year; tm_mon; tm_mday; tm_hour; tm_min; tm_sec; _} =
     Unix.gmtime (float_of_string s)
@@ -95,55 +90,59 @@ module Note : sig
   val migrate: Mantis.Note.t -> Github.Issue.Comment.t
 end = struct
   let migrate {Mantis.Note.reporter; text; last_modified = _; date_submitted} =
-    let reporter = match reporter with None -> "" | Some s -> s in
-    let text =
-      let buf = Buffer.create 101 in
-      badd buf "Reporter" reporter;
-      badd buf "Body" text;
-      Buffer.contents buf
+    let body =
+      let reporter =
+        match reporter with
+        | None -> ""
+        | Some s -> Printf.sprintf "**Original comment author:** %s\n" s
+      in
+      let text = String.trim text in
+      if text <> "" then reporter ^ "\n" ^ text else reporter
     in
-    {Github.Issue.Comment.body = text;
-     created_at = Some (timestamp date_submitted)}
+    {Github.Issue.Comment.body; created_at = Some (timestamp date_submitted)}
 end
 
 module Issue : sig
   val migrate: gh_user:(string -> string option) -> Mantis.Issue.t -> Github.Issue.t
 end = struct
-  let body ~id ?(reporter = "") ~tags ~category ~version ~target_version ~fixed_in_version
-      ~priority ~severity
-      ~description ~steps_to_reproduce ~additional_information ~related:_
+  let body
+      ~id ?(reporter = "") ~tags ~category
+      ~version ~target_version ~fixed_in_version
+      ~priority ~severity ~related:_
     =
-    let buf = Buffer.create 101 in
-    let info =
-      let combine l =
-        let l = List.map (fun (s1, s2) -> (s1, String.trim s2)) l in
-        let l = List.filter (function (_, "") -> false | _ -> true) l in
-        String.concat "\n" (List.map (fun (s1, s2) -> s1 ^ ": " ^ s2) l)
-      in
-      combine
-        [ "ID", string_of_int id;
-          "Reporter", reporter;
-          "Version", version;
-          "Target version", target_version;
-          "Fixed in version", fixed_in_version;
-          "Category", category;
-          "Priority", Mantis.Priority.to_string priority;
-          "Severity", Mantis.Severity.to_string severity;
-          "Tags", String.concat ", " tags ];
+    let combine l =
+      let l = List.map (fun (s1, s2) -> (s1, String.trim s2)) l in
+      let l = List.filter (function (_, "") -> false | _ -> true) l in
+      String.concat "\n" (List.map (fun (s1, s2) -> Printf.sprintf "**%s:** %s" s1 s2) l)
     in
-    badd buf "Original bug information" info;
-    badd buf "Description" description;
-    badd buf "Steps to reproduce" steps_to_reproduce;
-    badd buf "Additional information" additional_information;
-    Buffer.contents buf
+    combine
+      [ "ID", string_of_int id;
+        "Reporter", reporter;
+        "Version", version;
+        "Target version", target_version;
+        "Fixed in version", fixed_in_version;
+        "Category", category;
+        "Priority", Mantis.Priority.to_string priority;
+        "Severity", Mantis.Severity.to_string severity;
+        "Tags", String.concat ", " tags ]
+
+  let extra_notes ~created_at ~description ~steps_to_reproduce ~additional_information =
+    let note title contents l =
+      let title = "#### " ^ title in
+      match String.trim contents with
+      | "" -> l
+      | body ->
+        let body = Printf.sprintf "#### %s\n\n%s" title body in
+        {Github.Issue.Comment.created_at = Some created_at; body} :: l
+    in
+    note "Description" description
+      (note "Steps to reproduce" steps_to_reproduce
+         (note "Additional information" additional_information []))
 
   let labels ~priority ~severity ~category:_ ~status:_ ~resolution =
-    let l =
-      Label.L.of_priority priority @
-      Label.L.of_severity severity @
-      Label.L.of_resolution resolution
-    in
-    List.sort_uniq Stdlib.compare l |> List.map Label.to_string
+    Label.L.(of_priority priority @ of_severity severity @ of_resolution resolution)
+    |> List.sort_uniq Stdlib.compare
+    |> List.map Label.to_string
 
   let milestone ~target_version:_ =
     None
@@ -175,9 +174,10 @@ end = struct
     =
     let title = if summary = "" then "*no title*" else summary in
     let body =
-      body ~id ?reporter ~tags ~category ~version ~target_version ~fixed_in_version
-        ~priority ~severity
-        ~description ~steps_to_reproduce ~additional_information ~related
+      body
+        ~id ?reporter ~tags ~category
+        ~version ~target_version ~fixed_in_version
+        ~priority ~severity ~related
     in
     let labels = labels ~priority ~severity ~category ~status ~resolution in
     let milestone = milestone ~target_version in
@@ -195,6 +195,7 @@ end = struct
       | None ->
           None
     in
+    let extra_notes = extra_notes ~created_at ~description ~steps_to_reproduce ~additional_information in
     let issue =
       {Github.Issue.Issue.title; body;
        created_at = Some (timestamp created_at);
@@ -202,5 +203,5 @@ end = struct
        assignee; milestone; closed_at;
        closed = Some closed; labels}
     in
-    {Github.Issue.issue; comments = List.map Note.migrate notes}
+    {Github.Issue.issue; comments = extra_notes @ List.map Note.migrate notes}
 end
